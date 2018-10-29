e7221cb02d1fa9b1ad2404d6f1e22820977aaab7
longhowlam <longhowlam@gmail.com>
2018-10-29 09:25:08 +0100

    cc

--- CBS_shapes.R
+++ CBS_shapes.R
@@ -3,10 +3,13 @@ library(sp)
 library(maptools)
 library(dplyr)
 library(ggmap)
+library(leaflet)
 
 ### shape file van CBS OP buurt nivo, is ontzettend dicht op een kaart
 # https://www.cbs.nl/nl-nl/dossier/nederland-regionaal/geografische%20data/wijk-en-buurtkaart-2018
 tmp <- readShapeSpatial("Uitvoer_shape/buurt2018.shp")
+
+tmp <- readShapeSpatial("Uitvoer_shape/gem_2018.shp")
 
 #### Zet coordinatensysteem
 proj4string(tmp) <- CRS("+init=epsg:28992")
@@ -33,18 +36,20 @@ ggplot(tmp) +
 
 # met google maps 
 NLMap = ggmap(get_googlemap(center = c(4.8952,52.3702), scale=2, zoom=7), extent="normal")
-NLMap +  geom_polygon(
+NLMap +  geom_polygon( 
+  data = tmp,
   aes(x = long, y = lat, group = group),
-  color = "grey23", 
+  color = "grey", 
   fill = NA
 )
 
 
-### op leaflet maar dit is net te veel....
-leaflet(ProvNL) %>%
+### op leaflet maar dit is net te veel op buurt niveau
+leaflet(tmp) %>%
   addTiles() %>%
   addPolygons(
-    stroke = TRUE, weight = 1, fillOpacity = 0.15, smoothFactor = 0.15
+    stroke = TRUE, weight = 1, fillOpacity = 0.15, smoothFactor = 0.15,
+    popup = as.character(tmp$AANT_INW)
   )
 
 
